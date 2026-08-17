f3f6086b7e402ccd83520e372545a24d991d1c69
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2026-08-17 14:02:14 +0200

    enhancement: vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,20 @@ import path from "path";
 import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
-  ],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
   resolve: {
     alias: {
-     "@": path.resolve(__dirname, "./src"),
-     "@components": path.resolve(__dirname, "./src/components"),
-     "@types": path.resolve(__dirname, "./src/types"),
-     "@utils": path.resolve(__dirname, "./src/utils"),
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
     },
   },
   server: {
     port: 5173,
+    proxy: {
+      "/api": "http://localhost:8080",
+    },
     strictPort: true,
   },
 });
